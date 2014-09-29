0167ee6c3b69554c83b02bc485307a6ddd48780d
Nicholas Santos <nick.r.santos@gmail.com>
2014-09-29 17:45:02 +0000

    Changed how the working dir is set to be calculated based on the current path

--- scripts/local_vars.py
+++ scripts/local_vars.py
@@ -1,7 +1,7 @@
 import os
 
 #set working directory to TRS directory
-wd = r'C:\Users\ambell\Documents\TRS'
+wd = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # gets the path of *this file* and then gets the dir it's in and then gets the parent dir
 
 #file paths
 counties = os.path.join(wd, r"data\geodata.gdb\CA_Counties")
